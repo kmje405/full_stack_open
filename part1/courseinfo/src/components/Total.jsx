8c008b62e14259091fdfe646980028485bdc4435
kmje405 <kmje405@gmail.com>
2026-03-09 16:46:43 -0500

    initial set up of part1 project, plus exercises 1.1 and 1.2

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Total(props) {
+  return (
+    <p>
+      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+    </p>
+  );
+}
